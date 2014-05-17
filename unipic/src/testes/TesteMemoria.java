a2eb763f6aa69438e29bb03f1a0a820365a3e497
Ivan <ivan.i.n@hotmail.com>
2014-05-17 17:02:36 -0300

    Adicionado arquivo e primeira classe de testes

---
+++ unipic/src/testes/TesteMemoria.java
@@ -0,0 +1,24 @@
+package testes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TesteMemoria {
+
+	@Test
+	public void testGet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSTATUS() {
+		fail("Not yet implemented");
+	}
+
+}
