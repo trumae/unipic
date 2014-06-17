fd24f93fa07c64f9a6697f30560072ac693538a2
Ivan <ivan.i.n@hotmail.com>
2014-06-17 16:29:31 -0300

    SUBWF, SWAPF, XORLW, XORWF e testes

--- unipic/src/instrucoes/SUBWF.java
+++ unipic/src/instrucoes/SUBWF.java
@@ -4,14 +4,34 @@ import unipic.Instrucao;
 import unipic.Memoria;
 
 public class SUBWF extends Instrucao{
+	/***
+	 *  Inicia a instrução 
+	 * @param comando 0000 10df ffff
+	 */
 	@Override
 	public void setup(String comando){
-
-		//     ** Vide Pagina 54 do manual da PIC **
+		this.setD(Integer.parseInt(comando.substring(6,7),2)); // base binario = inteiro 
+		this.setF(Integer.parseInt(comando.substring(7),2)); // 
 	}
-		
+	
+	/***
+	 *  Subtrai o registrador 'w' do registrador 'f'
+	 *  Se 'd' é zero, o resultado é salvo em 'w', se 'd' é 1, o resultado é salvo em 'f'
+	 * @param mem memória a ser alterada
+	 * @param cpu serve para usar os registradores do processador
+	 */
 	public void run(Memoria mem, CPU cpu){
-
+		byte w = cpu.getW();
+		byte valorEmF = mem.get(this.f);
+		
+		byte resultado = (byte) (valorEmF - w);
+		
+		if(this.d==0){
+			cpu.setW(resultado);
+		} else {
+			mem.set(this.f,resultado);
+		}
+		mem.setPCL((byte) (mem.getPCL() + 1));
 	}
 }
 
