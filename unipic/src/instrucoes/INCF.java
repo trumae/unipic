5d7dd29aa69cdb43cf10b875a9859ade3e8b4640
Mickael <Mickael@Mickael-PC>
2014-06-18 16:57:55 -0300

    package instrucoes; import unipic.CPU; import unipic.Instrucao; import unipic.Memoria;
    public class INCF extends Instrucao{
    	/***
    	 *  Inicia a instrução 
    	 * comando 0010 10df ffff
    	 */
    	@Override
    	public void setup(String comando){
    		setD(Integer.parseInt(comando.substring(6,7),2));
    		setF(Integer.parseInt(comando.substring(7),2)); 
    	}
    
    	/***
    	 *  Os conteúdos do registro 'f' são incrementados
    	 *  Se o bit 'd' é '0', o resultado é colocado no registro 'W'
    	 *  Se o bit 'd' é '1', o resultado é colocado de volta no registro 'f'
    	 */
    	public void run(Memoria mem, CPU cpu){
    		 
    		byte novoF = mem.get(f);
    		novoF ++;
    		mem.set(f,novoF);
    		
    		if (this.d == 0){
    			
    			cpu.setW(novoF);
    			
    		}
    		
    		else {
    			
    			mem.set(f,novoF);
    			
    		}
    			
    		mem.setPCL((byte) (mem.getPCL() + 1));
    	}
    }

--- unipic/src/instrucoes/INCF.java
+++ unipic/src/instrucoes/INCF.java
@@ -4,13 +4,39 @@ import unipic.Instrucao;
 import unipic.Memoria;
 		
 public class INCF extends Instrucao{
+	/***
+	 *  Inicia a instrução 
+	 * comando 0010 10df ffff
+	 */
 	@Override
 	public void setup(String comando){
-		
-		//     ** Vide Pagina 53 do manual da PIC **
+		setD(Integer.parseInt(comando.substring(6,7),2));
+		setF(Integer.parseInt(comando.substring(7),2)); 
 	}
-	
-	public void run(Memoria mem, CPU cpu){
 
+	/***
+	 *  Os conteúdos do registro 'f' são incrementados
+	 *  Se o bit 'd' é '0', o resultado é colocado no registro 'W'
+	 *  Se o bit 'd' é '1', o resultado é colocado de volta no registro 'f'
+	 */
+	public void run(Memoria mem, CPU cpu){
+		 
+		byte novoF = mem.get(f);
+		novoF ++;
+		mem.set(f,novoF);
+		
+		if (this.d == 0){
+			
+			cpu.setW(novoF);
+			
+		}
+		
+		else {
+			
+			mem.set(f,novoF);
+			
+		}
+			
+		mem.setPCL((byte) (mem.getPCL() + 1));
 	}
 }
