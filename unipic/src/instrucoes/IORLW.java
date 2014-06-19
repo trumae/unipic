b0015f87ab2d7b5ba912dc9274119414c25a1b03
matheusmatos26 <matheus.epr@hotmail.com>
2014-06-18 21:58:25 -0300

    Update IORLW.java

--- unipic/src/instrucoes/IORLW.java
+++ unipic/src/instrucoes/IORLW.java
@@ -4,13 +4,25 @@ import unipic.Instrucao;
 import unipic.Memoria;
 
 public class IORLW  extends Instrucao{
+	
+	/***
+	 *  Inicia a instrução 
+	 * @param comando 1101 kkkk kkkk
+	 **/
 	@Override
 	public void setup(String comando){
 		
-		//     ** Vide Pagina 54 do manual da PIC **
+		setK(Integer.parseInt(comando.substring(4), 2));
 	}
 	
+	/***
+	 *  Faco um operacao 'OR' entre o 'k' e o 'W'
+	 * @param mem memória a ser alterada
+	 * @param cpu serve para usar os registradores do processador
+	 */
 	public void run(Memoria mem, CPU cpu){
-
+		
+		cpu.setW((byte) (cpu.getW() | this.k));
+		mem.setPCL((byte) (mem.getPCL() + 1));
 	}
 }
