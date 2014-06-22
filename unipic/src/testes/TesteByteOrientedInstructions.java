c3682aa5ffa25ff86a5eabc5b33e252cefd6957b
rturquetti <Rafael@Rafa>
2014-06-22 14:18:03 -0300

    Instrução Goto e testes

--- unipic/src/testes/TesteByteOrientedInstructions.java
+++ unipic/src/testes/TesteByteOrientedInstructions.java
@@ -124,4 +124,310 @@ public class TesteByteOrientedInstructions extends TestCase{
 			fail("Erro na 2° instrucao, esperado: f=-30, obtido: "+mem.get(14));
 		}
 	}
+	
+	public void testeADDWF(){
+		Memoria mem = new Memoria();
+ 		CPU cpu = new CPU();
+ 		
+ 		mem.set(15, (byte)0);
+ 		mem.set(14, (byte)10);
+ 		mem.set(13, (byte)20);
+ 		//Seta o valor de w. w = 0
+ 		cpu.setW((byte)00000000);
+ 		
+ 		Instrucao i1 = new ADDWF();
+		i1.setup("000111000000"); //f=0 
+ 		Instrucao i2 = new ADDWF();
+		i2.setup("000111001010"); //f=10 
+ 		Instrucao i3 = new ADDWF();
+		i3.setup("000111010100"); //f=20 
+ 		
+ 		i1.run(mem, cpu);
+ 		if(mem.get(15) != 0){
+ 			fail("Erro na 1a Instrucao, esperado: 0 , obtido: "+mem.get(15));
+ 		}
+ 		
+ 		i2.run(mem, cpu);
+ 		if(mem.get(14) != 10){
+ 			fail("Erro na 1a Instrucao, esperado: 10 , obtido: "+mem.get(14));
+ 		}
+ 		
+ 		i1.run(mem, cpu);
+ 		if(mem.get(13) != 20){
+ 			fail("Erro na 1a Instrucao, esperado: 20 , obtido: "+mem.get(13));
+ 		}
+	}
+	
+	public void testeIORLW(){
+		Memoria mem = new Memoria();
+ 		CPU cpu = new CPU();
+ 		
+ 		mem.set(15, (byte)0);
+ 		mem.set(14, (byte)29);
+ 		mem.set(13, (byte)58);
+ 		//Seta o valor de w. w = 29
+ 		cpu.setW((byte)00011101);
+ 		
+ 		Instrucao i1 = new IORLW();
+		i1.setup("000100000000"); //f=0 
+ 		Instrucao i2 = new IORLW();
+		i2.setup("000100000001"); //f=1 
+ 		Instrucao i3 = new IORLW();
+		i3.setup("000100000010"); //f=2 
+ 		
+ 		i1.run(mem, cpu);
+ 		//w*f -> 80*0=0
+ 		if(mem.get(15) != 0){
+ 			fail("Erro na 1a Instrucao, esperado: 0 , obtido: "+mem.get(15));
+ 		}
+ 		
+ 		i2.run(mem, cpu);
+ 		//w*f -> 80*1=80
+ 		if(mem.get(14) != 29){
+ 			fail("Erro na 1a Instrucao, esperado: 29 , obtido: "+mem.get(14));
+ 		}
+ 		
+ 		i1.run(mem, cpu);
+ 		//w*f -> 80*2=160
+ 		if(mem.get(13) != 58){
+ 			fail("Erro na 1a Instrucao, esperado: 58 , obtido: "+mem.get(13));
+ 		}
+	}
+	
+	public void testeMOVLW(){
+		Memoria mem = new Memoria();
+ 		CPU cpu = new CPU();
+ 		
+ 		mem.set(15, (byte)127);
+ 		mem.set(14, (byte)0);
+ 		mem.set(13, (byte)50);
+ 		
+ 		Instrucao i1 = new MOVLW();
+		i1.setup("110001111111"); //f=127 
+ 		Instrucao i2 = new MOVLW();
+		i2.setup("110000000000"); //f=0 
+ 		Instrucao i3 = new MOVLW();
+		i3.setup("110000110010"); //f=50
+ 		
+ 		i1.run(mem, cpu);
+ 		if(mem.get(15) != 127){
+ 			fail("Erro na 1a Instrucao, esperado: 127 , obtido: "+mem.get(15));
+ 		}
+ 		
+ 		i2.run(mem, cpu);
+ 		if(mem.get(14) != 0){
+ 			fail("Erro na 1a Instrucao, esperado: 0 , obtido: "+mem.get(14));
+ 		}
+ 		
+ 		i3.run(mem, cpu);
+ 		if(mem.get(13) != 50){
+ 			fail("Erro na 1a Instrucao, esperado: 50 , obtido: "+mem.get(13));
+ 		}
+	}
+	
+	public void testANDWF(){
+		Memoria mem = new Memoria();
+ 		CPU cpu = new CPU();
+ 		// seta w=55
+ 		cpu.setW((byte)01011001);
+ 		
+ 		mem.set(15, (byte)70);
+ 		mem.set(14, (byte)57);
+ 		mem.set(13, (byte)62);
+
+ 		
+ 		Instrucao i1 = new ANDWF();
+		i1.setup("010100001111"); //f=15
+		Instrucao i2 = new ANDWF();
+		i2.setup("010100000010"); //f=2
+		Instrucao i3 = new ANDWF();
+		i3.setup("010100000111"); //f=7
+		
+ 		i1.run(mem, cpu);
+ 		if(mem.get(15) != 70){
+ 			fail("Erro na 1a Instrucao, esperado: 70 , obtido: "+mem.get(15));
+ 		}
+ 		
+ 		i2.run(mem, cpu);
+ 		if(mem.get(14) != 57){
+ 			fail("Erro na 1a Instrucao, esperado: 57 , obtido: "+mem.get(14));
+ 		}
+ 		
+ 		i3.run(mem, cpu);
+ 		if(mem.get(13) != 62){
+ 			fail("Erro na 1a Instrucao, esperado: 62 , obtido: "+mem.get(13));
+ 		}
+	}
+	
+	public void testCLRF(){
+		Memoria mem = new Memoria();
+ 		CPU cpu = new CPU();
+		
+ 		mem.set(15, (byte)0);
+ 		mem.set(14, (byte)0);
+ 		mem.set(13, (byte)0);
+		
+		Instrucao i1 = new CLRF();
+ 		i1.setup("000001100001"); // f=1
+		Instrucao i2 = new CLRF();
+ 		i2.setup("000001100010"); // f=2
+		Instrucao i3 = new CLRF();
+ 		i3.setup("000001100101"); // f=5
+ 		
+ 		i1.run(mem, cpu);
+ 		if(mem.get(15) != 0){
+ 			fail("Erro na 1a Instrucao, esperado: 0 , obtido: "+mem.get(15));
+ 		}
+ 		
+ 		i2.run(mem, cpu);
+ 		if(mem.get(14) != 0){
+ 			fail("Erro na 1a Instrucao, esperado: 0 , obtido: "+mem.get(14));
+ 		}
+ 		
+ 		i3.run(mem, cpu);
+ 		if(mem.get(13) != 0){
+ 			fail("Erro na 1a Instrucao, esperado: 0 , obtido: "+mem.get(13));
+ 		}
+	}
+	
+	public void testCLRW(){
+		Memoria mem = new Memoria();
+ 		CPU cpu = new CPU();
+		
+ 		mem.set(15, (byte)0);
+ 		mem.set(14, (byte)0);
+ 		mem.set(13, (byte)0);
+ 		
+ 		Instrucao i1 = new CLRW();
+ 		i1.setup("000001000000");
+ 		
+ 		i1.run(mem, cpu);
+ 		if(mem.get(15) != 0){
+ 			fail("Erro na 1a Instrucao, esperado: 0 , obtido: "+mem.get(15));
+		}
+	}
+	
+	public void testDECF(){
+		Memoria mem = new Memoria();
+ 		CPU cpu = new CPU();
+		
+ 		mem.set(15, (byte)14);	
+ 		
+ 		Instrucao i1 = new DECF();
+ 		i1.setup("001100001111");
+ 		
+ 		i1.run(mem, cpu); 		
+		if(mem.get(15) != 14){
+ 			fail("Erro na 1a Instrucao, esperado: 14 , obtido: "+mem.get(15));
+		}
+	}
+	
+	public void testCOMF(){
+	Memoria mem = new Memoria();
+		CPU cpu = new CPU();
+
+		mem.set(15, (byte)241);	
+		
+		Instrucao i1 = new COMF();
+		i1.setup("100100001111");
+		
+		i1.run(mem, cpu);
+		if(mem.get(15) != -15){
+			fail("Erro na 1a Instrucao, esperado: -15 , obtido: "+mem.get(15));
+		}
+	}
+	
+	public void testDECFSZ(){
+		Memoria mem = new Memoria();
+ 		CPU cpu = new CPU();
+		
+ 		mem.set(15, (byte)14);	
+ 		mem.set(14, (byte)6);	
+ 		mem.set(13, (byte)2);	
+ 		
+ 		Instrucao i1 = new DECFSZ();
+ 		i1.setup("101100001111"); //f=15
+ 		Instrucao i2 = new DECFSZ();
+ 		i2.setup("101100000111"); //f=7
+ 		Instrucao i3 = new DECFSZ();
+ 		i3.setup("101100000011"); //f=3
+ 		
+ 		i1.run(mem, cpu);
+ 		if(mem.get(15) != 14){
+ 			fail("Erro na 1a Instrucao, esperado: 14 , obtido: "+mem.get(15));
+		}
+ 		
+ 		i2.run(mem, cpu);
+ 		if(mem.get(14) != 6){
+ 			fail("Erro na 1a Instrucao, esperado: 6 , obtido: "+mem.get(14));
+		}
+ 		
+ 		i3.run(mem, cpu);
+ 		if(mem.get(13) != 2){
+ 			fail("Erro na 1a Instrucao, esperado: 3 , obtido: "+mem.get(13));
+		}
+	}
+	
+	public void testINCFSZ(){
+		Memoria mem = new Memoria();
+ 		CPU cpu = new CPU();
+		
+ 		mem.set(15, (byte)16);	
+ 		mem.set(14, (byte)8);
+ 		mem.set(13, (byte)4);
+ 		
+ 		Instrucao i1 = new DECFSZ();
+ 		i1.setup("001111001111"); //f=15
+ 		Instrucao i2 = new DECFSZ();
+ 		i2.setup("001111000111"); //f=7
+ 		Instrucao i3 = new DECFSZ();
+ 		i3.setup("001111000011"); //f=3
+ 		
+ 		i1.run(mem, cpu);
+ 		if(mem.get(15) != 16){
+ 			fail("Erro na 1a Instrucao, esperado: 16 , obtido: "+mem.get(15));
+		}
+ 		
+ 		i2.run(mem, cpu);
+ 		if(mem.get(14) != 8){
+ 			fail("Erro na 1a Instrucao, esperado: 8 , obtido: "+mem.get(14));
+		}
+ 		
+ 		i3.run(mem, cpu);
+ 		if(mem.get(13) != 4){
+ 			fail("Erro na 1a Instrucao, esperado: 4 , obtido: "+mem.get(13));
+		}
+	}
+	
+	public void testMOVF(){
+		Memoria mem = new Memoria();
+ 		CPU cpu = new CPU();
+		
+ 		mem.set(15, (byte)15);	
+ 		mem.set(14, (byte)7);	
+ 		mem.set(13, (byte)3);	
+ 		
+ 		Instrucao i1 = new MOVF();
+ 		i1.setup("001000001111"); // f=15
+ 		Instrucao i2 = new MOVF();
+ 		i1.setup("001000001111"); // f=7
+ 		Instrucao i3 = new MOVF();
+ 		i1.setup("001000001111"); // f=3
+ 		
+ 		i1.run(mem, cpu);
+ 		if(mem.get(15) != 15){
+ 			fail("Erro na 1a Instrucao, esperado: 16 , obtido: "+mem.get(15));
+		}
+ 		
+ 		i1.run(mem, cpu);
+ 		if(mem.get(14) != 7){
+ 			fail("Erro na 1a Instrucao, esperado: 7 , obtido: "+mem.get(14));
+		}
+ 		
+ 		i1.run(mem, cpu);
+ 		if(mem.get(13) != 3){
+ 			fail("Erro na 1a Instrucao, esperado: 3 , obtido: "+mem.get(13));
+		}
+	}
+	
 }
