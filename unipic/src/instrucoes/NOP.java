d1f6b5284abf8b8a9a87118b16ba0f8bcc42b7b0
eduardoValle <luizeduardovalle@gmail.com>
2014-06-22 23:28:15 -0300

    NOP

--- unipic/src/instrucoes/NOP.java
+++ unipic/src/instrucoes/NOP.java
@@ -1,19 +1,35 @@
 package instrucoes;
+
 import unipic.CPU;
 import unipic.Instrucao;
 import unipic.Memoria;
 
 public class NOP extends Instrucao{
+	
+	/***
+	 * 
+	 *  Inicia a instrucao 
+	 * @param comando 0000 0000 0000
+	 * 
+	 **/
 	@Override
 	public void setup(String comando){
-
-		//     ** Vide Pagina 54 do manual da PIC **
 	}
 	
+	/***
+	 * 
+	 * Esse método faz simplesmente nada =D
+	 * aqui, só incremento o PCL
+	 * @param mem memoria a ser alterada
+	 * @param cpu serve para usar os registradores do processador
+	 * 
+	 */
 	public void run(Memoria mem, CPU cpu){
-		// incrementa PCL
+		
 		mem.setPCL((byte) (mem.getPCL() + 1));
 	}
 }
+
+
 
 
